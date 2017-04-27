148bf3ed8154f0b4d294342931567f7dad9d946b
Manuel Leduc <manuel.leduc@inria.fr>
2017-04-27 08:43:44 +0200

    delegates ok

--- ale.compiler/src/ale/compiler/generator/GenerateOperationImplXtend.xtend
+++ ale.compiler/src/ale/compiler/generator/GenerateOperationImplXtend.xtend
@@ -6,6 +6,7 @@ import ale.xtext.ale.AleClass
 import ale.xtext.ale.Root
 import java.util.List
 import org.eclipse.emf.ecore.EPackage
+import ale.xtext.ale.Method
 
 class GenerateOperationImplXtend {
 
@@ -21,8 +22,7 @@ class GenerateOperationImplXtend {
 	}
 
 	def String generate(EClass eClass, AleClass aleClass, List<EPackage> ePackages, Root root) {
-		val aleName = if(aleClass != null) (aleClass.eContainer as Root).
-				name else "void"
+		val aleName = aleClass.aleRootName
 
 		val clazzName = '''«aleName.toFirstUpper»«eClass.name»Operation'''
 		'''
@@ -33,13 +33,22 @@ class GenerateOperationImplXtend {
 			
 			private final «eClass.javaFullPath» self;
 			private final «aleName».revisitor.«aleName.toFirstUpper»Revisitor alg;
+			
+			
+			«FOR parent: aleClass.superClass»
+			private final «parent.aleRootName».revisitor.operation.impl.«parent.aleRootName.toFirstUpper»«parent.name.toFirstUpper»OperationImpl «parent.aleRootName»delegate;
+			«ENDFOR»
+			
 			public «clazzName»Impl(«eClass.javaFullPath» self, «aleName».revisitor.«aleName.toFirstUpper»Revisitor alg) {
 				this.self = self;
 				this.alg = alg;
+				
+				«FOR parent: aleClass.superClass»
+				this.«parent.aleRootName»delegate = new «parent.aleRootName».revisitor.operation.impl.«parent.aleRootName.toFirstUpper»«parent.name.toFirstUpper»OperationImpl(self, alg);
+				«ENDFOR»
 			}
-«««			TODO: Dealing with inherited methods !!!
 			«IF aleClass != null»
-			«FOR method: aleClass.methods»
+			«FOR method: aleClass.methodsRec»
 			@Override
 			public «method.type.solveStaticType(ePackages)» «method.name»(«FOR p: method.params»«p.type.solveStaticType(ePackages)» «p.name»«ENDFOR») {
 				«IF method.type != null»
@@ -49,5 +58,26 @@ class GenerateOperationImplXtend {
 			«ENDFOR»
 			«ENDIF»
 		}'''
+	}
+	
+	def List<Method> methodsRec(AleClass aleClass) {
+		val List<Method> ret = newArrayList(aleClass.methods)
+		for(AleClass parent: aleClass.superClass) {
+			// getAll parents methods
+			val tmp = parent.methodsRec
+			for(Method tmpM: tmp) {
+				
+				// only add missing ones
+				if(!ret.exists[it.name == tmpM.name && it.params.size == tmpM.params.size]) {
+					ret.add(tmpM)
+				}		
+			}
+		}
+		ret
+	}
+	
+	def String getAleRootName(AleClass aleClass) {
+		if(aleClass != null) (aleClass.eContainer as Root).
+				name else "void"
 	}
 }
--- ale.compiler/xtend-gen/ale/compiler/generator/GenerateOperationImplXtend.java
+++ ale.compiler/xtend-gen/ale/compiler/generator/GenerateOperationImplXtend.java
@@ -16,7 +16,11 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.StringExtensions;
 
 @SuppressWarnings("all")
@@ -43,15 +47,7 @@ public class GenerateOperationImplXtend {
   public String generate(final EClass eClass, final AleClass aleClass, final List<EPackage> ePackages, final Root root) {
     String _xblockexpression = null;
     {
-      String _xifexpression = null;
-      boolean _notEquals = (!Objects.equal(aleClass, null));
-      if (_notEquals) {
-        EObject _eContainer = aleClass.eContainer();
-        _xifexpression = ((Root) _eContainer).getName();
-      } else {
-        _xifexpression = "void";
-      }
-      final String aleName = _xifexpression;
+      final String aleName = this.getAleRootName(aleClass);
       StringConcatenation _builder = new StringConcatenation();
       String _firstUpper = StringExtensions.toFirstUpper(aleName);
       _builder.append(_firstUpper, "");
@@ -91,6 +87,33 @@ public class GenerateOperationImplXtend {
       _builder_1.append("Revisitor alg;");
       _builder_1.newLineIfNotEmpty();
       _builder_1.append("\t");
+      _builder_1.newLine();
+      _builder_1.append("\t");
+      _builder_1.newLine();
+      {
+        EList<AleClass> _superClass = aleClass.getSuperClass();
+        for(final AleClass parent : _superClass) {
+          _builder_1.append("\t");
+          _builder_1.append("private final ");
+          String _aleRootName = this.getAleRootName(parent);
+          _builder_1.append(_aleRootName, "\t");
+          _builder_1.append(".revisitor.operation.impl.");
+          String _aleRootName_1 = this.getAleRootName(parent);
+          String _firstUpper_2 = StringExtensions.toFirstUpper(_aleRootName_1);
+          _builder_1.append(_firstUpper_2, "\t");
+          String _name_1 = parent.getName();
+          String _firstUpper_3 = StringExtensions.toFirstUpper(_name_1);
+          _builder_1.append(_firstUpper_3, "\t");
+          _builder_1.append("OperationImpl ");
+          String _aleRootName_2 = this.getAleRootName(parent);
+          _builder_1.append(_aleRootName_2, "\t");
+          _builder_1.append("delegate;");
+          _builder_1.newLineIfNotEmpty();
+        }
+      }
+      _builder_1.append("\t");
+      _builder_1.newLine();
+      _builder_1.append("\t");
       _builder_1.append("public ");
       _builder_1.append(clazzName, "\t");
       _builder_1.append("Impl(");
@@ -99,8 +122,8 @@ public class GenerateOperationImplXtend {
       _builder_1.append(" self, ");
       _builder_1.append(aleName, "\t");
       _builder_1.append(".revisitor.");
-      String _firstUpper_2 = StringExtensions.toFirstUpper(aleName);
-      _builder_1.append(_firstUpper_2, "\t");
+      String _firstUpper_4 = StringExtensions.toFirstUpper(aleName);
+      _builder_1.append(_firstUpper_4, "\t");
       _builder_1.append("Revisitor alg) {");
       _builder_1.newLineIfNotEmpty();
       _builder_1.append("\t\t");
@@ -109,15 +132,38 @@ public class GenerateOperationImplXtend {
       _builder_1.append("\t\t");
       _builder_1.append("this.alg = alg;");
       _builder_1.newLine();
+      _builder_1.append("\t\t");
+      _builder_1.newLine();
+      {
+        EList<AleClass> _superClass_1 = aleClass.getSuperClass();
+        for(final AleClass parent_1 : _superClass_1) {
+          _builder_1.append("\t\t");
+          _builder_1.append("this.");
+          String _aleRootName_3 = this.getAleRootName(parent_1);
+          _builder_1.append(_aleRootName_3, "\t\t");
+          _builder_1.append("delegate = new ");
+          String _aleRootName_4 = this.getAleRootName(parent_1);
+          _builder_1.append(_aleRootName_4, "\t\t");
+          _builder_1.append(".revisitor.operation.impl.");
+          String _aleRootName_5 = this.getAleRootName(parent_1);
+          String _firstUpper_5 = StringExtensions.toFirstUpper(_aleRootName_5);
+          _builder_1.append(_firstUpper_5, "\t\t");
+          String _name_2 = parent_1.getName();
+          String _firstUpper_6 = StringExtensions.toFirstUpper(_name_2);
+          _builder_1.append(_firstUpper_6, "\t\t");
+          _builder_1.append("OperationImpl(self, alg);");
+          _builder_1.newLineIfNotEmpty();
+        }
+      }
       _builder_1.append("\t");
       _builder_1.append("}");
       _builder_1.newLine();
       {
-        boolean _notEquals_1 = (!Objects.equal(aleClass, null));
-        if (_notEquals_1) {
+        boolean _notEquals = (!Objects.equal(aleClass, null));
+        if (_notEquals) {
           {
-            EList<Method> _methods = aleClass.getMethods();
-            for(final Method method : _methods) {
+            List<Method> _methodsRec = this.methodsRec(aleClass);
+            for(final Method method : _methodsRec) {
               _builder_1.append("\t");
               _builder_1.append("@Override");
               _builder_1.newLine();
@@ -127,8 +173,8 @@ public class GenerateOperationImplXtend {
               String _solveStaticType = this.typeUtil.solveStaticType(_type, ePackages);
               _builder_1.append(_solveStaticType, "\t");
               _builder_1.append(" ");
-              String _name_1 = method.getName();
-              _builder_1.append(_name_1, "\t");
+              String _name_3 = method.getName();
+              _builder_1.append(_name_3, "\t");
               _builder_1.append("(");
               {
                 EList<Param> _params = method.getParams();
@@ -137,16 +183,16 @@ public class GenerateOperationImplXtend {
                   String _solveStaticType_1 = this.typeUtil.solveStaticType(_type_1, ePackages);
                   _builder_1.append(_solveStaticType_1, "\t");
                   _builder_1.append(" ");
-                  String _name_2 = p.getName();
-                  _builder_1.append(_name_2, "\t");
+                  String _name_4 = p.getName();
+                  _builder_1.append(_name_4, "\t");
                 }
               }
               _builder_1.append(") {");
               _builder_1.newLineIfNotEmpty();
               {
                 Type _type_2 = method.getType();
-                boolean _notEquals_2 = (!Objects.equal(_type_2, null));
-                if (_notEquals_2) {
+                boolean _notEquals_1 = (!Objects.equal(_type_2, null));
+                if (_notEquals_1) {
                   _builder_1.append("\t");
                   _builder_1.append("\t");
                   _builder_1.append("return null;");
@@ -164,5 +210,43 @@ public class GenerateOperationImplXtend {
       _xblockexpression = _builder_1.toString();
     }
     return _xblockexpression;
+  }
+  
+  public List<Method> methodsRec(final AleClass aleClass) {
+    List<Method> _xblockexpression = null;
+    {
+      EList<Method> _methods = aleClass.getMethods();
+      final List<Method> ret = CollectionLiterals.<Method>newArrayList(((Method[])Conversions.unwrapArray(_methods, Method.class)));
+      EList<AleClass> _superClass = aleClass.getSuperClass();
+      for (final AleClass parent : _superClass) {
+        {
+          final List<Method> tmp = this.methodsRec(parent);
+          for (final Method tmpM : tmp) {
+            final Function1<Method, Boolean> _function = (Method it) -> {
+              return Boolean.valueOf((Objects.equal(it.getName(), tmpM.getName()) && (it.getParams().size() == tmpM.getParams().size())));
+            };
+            boolean _exists = IterableExtensions.<Method>exists(ret, _function);
+            boolean _not = (!_exists);
+            if (_not) {
+              ret.add(tmpM);
+            }
+          }
+        }
+      }
+      _xblockexpression = ret;
+    }
+    return _xblockexpression;
+  }
+  
+  public String getAleRootName(final AleClass aleClass) {
+    String _xifexpression = null;
+    boolean _notEquals = (!Objects.equal(aleClass, null));
+    if (_notEquals) {
+      EObject _eContainer = aleClass.eContainer();
+      _xifexpression = ((Root) _eContainer).getName();
+    } else {
+      _xifexpression = "void";
+    }
+    return _xifexpression;
   }
 }
