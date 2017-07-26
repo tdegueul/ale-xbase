4669f32c22fb9a073c123a83c972a27b996cedd8
Thomas Degueule <degueule@cwi.nl>
2017-07-26 23:01:31 +0200

    Added BoolExp testadata project

---
+++ testdata/boolexp/src/boolexp/Or.java
@@ -0,0 +1,17 @@
+/**
+ */
+package boolexp;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Or</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see boolexp.BoolexpPackage#getOr()
+ * @model
+ * @generated
+ */
+public interface Or extends BinaryExp {
+} // Or
---
+++ testdata/boolexp/src/boolexp/impl/FalsImpl.java
@@ -0,0 +1,37 @@
+/**
+ */
+package boolexp.impl;
+
+import boolexp.BoolexpPackage;
+import boolexp.Fals;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Fals</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class FalsImpl extends LitImpl implements Fals {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected FalsImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return BoolexpPackage.Literals.FALS;
+	}
+
+} //FalsImpl
